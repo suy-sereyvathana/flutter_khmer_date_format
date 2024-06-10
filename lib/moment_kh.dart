6e256a167de27dfb06e30cf1a158264baf97faf3
suy-sereyvathana <suysereyvathana@gmail.com>
2024-06-10 16:54:29 +0700

    update

---
+++ lib/moment_kh.dart
@@ -0,0 +1,13 @@
+import 'package:khmer_date_format/constants/constant_variables.dart';
+
+class MomentKH {
+  static Map<String, dynamic> getKhmerLunarString(DateTime date) {
+    List<String> hsMonth = ConstantVariables.khmerMonths;
+
+    List<String> hsAnimalYear = ConstantVariables.animalYears;
+
+    List<String> hsSak = ConstantVariables.eraYear;
+
+    return {"khDayAMs": 123};
+  }
+}
