f86ed94387927ff9115ac025ec76a032f797654b
suy-sereyvathana <suysereyvathana@gmail.com>
2024-06-10 11:29:51 +0700

    update

--- lib/latin_date_format.dart
+++ lib/latin_date_format.dart
@@ -1,16 +1,23 @@
 import 'package:khmer_date_format/constants/constant_functions.dart';
 import 'package:khmer_date_format/constants/constant_variables.dart';
 
-class LatinDateFormat{
-  static DateTime latinFormat(String date){
+class LatinDateFormat {
+  static DateTime latinFormat(String date) {
     DateTime latinDate = DateTime.now();
+
     List dateArray = date.split('-').toList();
+
     int? day = ConstantFunctions().reverseKhNum(dateArray[0].toString());
-    int? month = ConstantVariables.khmerMonths.indexOf(dateArray[1],0).toInt()+1;
+
+    int? month =
+        ConstantVariables.khmerMonths.indexOf(dateArray[1], 0).toInt() + 1;
+
     int? year = ConstantFunctions().reverseKhNum(dateArray[2]);
-    if(day!= null && month!= null && year!= null){
-      latinDate = DateTime(year,month,day);
+
+    if (day != null && month != null && year != null) {
+      latinDate = DateTime(year, month, day);
     }
+
     return latinDate;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,2 @@
-import 'package:khmer_date_format/latin_date_format.dart';
-import 'package:khmer_date_format/moment_kh.dart';
-
 export "khmer_date_format.dart";
 export "constants/constant_functions.dart";
-
-void main(List<String> args) {
-  DateTime date = new DateTime.now();
-  DateTime newdate = LatinDateFormat.latinFormat('១៧-ឧសភា-២០២៤');
-  int year = newdate.year;
-  int month = newdate.month;
-  int day = newdate.day;
-  print(year);
-  print(month);
-  print(day);
-  print(newdate);
-}
